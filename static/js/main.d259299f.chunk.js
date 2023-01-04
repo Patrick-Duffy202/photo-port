16a569a65a05546cfd8dbffdb8ddc244528bd0ec
Patrick-Duffy202 <Patrickduffy202@gmail.com>
2023-01-04 15:20:10 -0600

    Updates

---
+++ static/js/main.d259299f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{10:function(t,n,e){"use strict";e.r(n);var o=e(0),c=e.n(o),i=e(4),r=e.n(i),s=(e(3),e.p,e(1));var u=function(){return Object(s.jsx)("div",{})},a=function(t){t&&t instanceof Function&&e.e(3).then(e.bind(null,11)).then((function(n){var e=n.getCLS,o=n.getFID,c=n.getFCP,i=n.getLCP,r=n.getTTFB;e(t),o(t),c(t),i(t),r(t)}))};r.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(u,{})}),document.getElementById("root")),a()},3:function(t,n,e){}},[[10,1,2]]]);
+//# sourceMappingURL=main.d259299f.chunk.js.map
